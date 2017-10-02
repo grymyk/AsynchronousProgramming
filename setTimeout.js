ca48d28983503fbce26488078d58b3dd9087fdbe
Mykola Grytsai <grymyk@gmail.com>
2017-10-02 22:15:36 +0300

    addData fn

--- setTimeout.js
+++ setTimeout.js
@@ -9,20 +9,33 @@ api.array20 = new Array(20);
 
 api.result = function result(n) {
 	console.log(' sum: ', n);
-}
+};
+
+api.addData = function addData() {
+	//api.sum += api.addend;
+	//console.log(api.sum);
+	//var len = api.sum;
+	//console.log('api len: ', len);
+
+	api.array = api.array.concat(api.array20);
+	console.log('api ', api.array);
+	var len = api.array.length;
+};
 
 api.add = function add(max, callback) {
 	//console.log(max);
 	//console.log(callback);
 
 	//api.sum += api.addend;
+	//console.log(api.sum);
+	//var len = api.sum;
+	//console.log('api len: ', len);
+
 	api.array = api.array.concat(api.array20);
 	console.log('api ', api.array);
-
-    //console.log(api.sum);
-	//var len = api.sum;
 	var len = api.array.length;
-	//console.log('api len: ', len);
+
+	//var len = api.addData();
 
     if (len < max) {
     	setTimeout(api.add, api.delay, max, callback);
@@ -33,22 +46,22 @@ api.add = function add(max, callback) {
 
 		clearTimeout(api.timer);
     }
-}
+};
 
 api.getSum = function getSum(max, callback) {
 	api.timer = setTimeout(api.add, api.delay, max, callback);
-}
+};
 
 var app = {};
  
 app.getData = function getData(size, callback) {
 	api.getSum(size, callback);
-}
+};
 
 app.build = function build(array) {
 	console.log(' len: ', array.length);
 	console.log(' Build ', array.toString() );
-}
+};
 
 app.size = 50;
 
